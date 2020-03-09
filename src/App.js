dcf791727f4a7b1794ddd743e84c99496b028bb4
Franklin M Correia <franklinmatos@gmail.com>
2020-03-09 12:50:59 -0300

    aula sobre rendereização condicional

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import Simples from './componentes/Simples'
+import ParImpar from './componentes/ParImpar'
 
 export default class App extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                {/* <Text style={styles.f20}>App!</Text> */
-                    <Simples texto='Compomente Simples!!!' />
+            <View style={styles.container} >
+                {
+                    //  <Text style={styles.f20}>App!</Text> 
+                    // <Simples texto='Compomente Simples!'></Simples>
+                    <ParImpar numero={36}> </ParImpar>
                 }
             </View>
         )
